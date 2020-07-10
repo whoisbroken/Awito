e16b19db772bf677af9578c893d5eb5651eef324
Alexander Petriga <cazhqa666@gmail.com>
2020-07-10 17:26:02 +0300

    increased close functional/save form info/valid

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,67 @@
 'use strict';
 
 const modalAdd = document.querySelector('.modal__add'),
-    modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-    modalSubmit = document.querySelector('.modal__submit'),
-    addAd = document.querySelector('.add__ad');
+  modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+  modalSubmit = document.querySelector('.modal__submit'),
+  catalog = document.querySelector('.catalog'),
+  modalItem = document.querySelector('.modal__item'),
+  modalBtnWarning = document.querySelector('.modal__btn-warning'),
+  addAd = document.querySelector('.add__ad');
 
+let dataBase = [];
 
-addAd.addEventListener('click', () => {
-    modalAdd.classList.remove('hide');
-    modalBtnSubmit.disabled = true;
+const elementsModalSubmit = [...modalSubmit.elements].filter(elem => elem.tagName !== 'BUTTON');
+
+const closeModal = function(event) {
+  const target = event.target;
+
+  if (target.closest('.modal__close') || target === this) {
+      this.classList.add('hide');
+      if (this === modalAdd) {
+          modalSubmit.reset();
+      }
+  }
+}
+
+const closeModalEscape = event => {
+  if (event.code === 'Escape') {
+    modalAdd.classList.add('hide');
+    modalItem.classList.add('hide');
+    document.removeEventListener('keydown', closeModalEscape)
+  }
+}
+
+modalSubmit.addEventListener('input', () => {
+  const validForm = elementsModalSubmit.every(elem => elem.value);
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? 'none' : '';
 })
 
-modalAdd.addEventListener('click', event => {
-    const target = event.target;
-    
-    if (target.classList.contains('modal__close') ||
-        target === modalAdd){
-        modalAdd.classList.add('hide');
-        modalSubmit.reset();
-    }    
+modalSubmit.addEventListener('submit', event => {
+  event.preventDefault();
+  const itemObj = {};
+  for (const elem of elementsModalSubmit) {
+    itemObj[elem.name] = elem.value;
+  }
+  dataBase.push(itemObj);
+})
+
+addAd.addEventListener('click', () => {
+  modalAdd.classList.remove('hide');
+  modalBtnSubmit.disabled = true;
+  document.addEventListener('keydown', closeModalEscape)
+})
+
+
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+
+catalog.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.closest('.card')) {
+    modalItem.classList.remove('hide');
+    document.addEventListener('keydown', closeModalEscape)
+  }
 })
